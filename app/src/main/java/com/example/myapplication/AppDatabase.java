f4e663edf8aaf9ca41a913b983650591fb4fc60e
枫叶 <2965388697@qq.com>
2024-11-20 17:22:16 +0800

    test1.1

--- app/src/main/java/com/example/myapplication/AppDatabase.java
+++ app/src/main/java/com/example/myapplication/AppDatabase.java
@@ -5,19 +5,22 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {BookProgress.class}, version = 1)
+@Database(entities = {Book.class, Chapter.class, BookProgress.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
-
-    public abstract BookProgressDao bookProgressDao();
 
     public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, "book_progress_db")
+                            AppDatabase.class, "app_database")
                     .fallbackToDestructiveMigration()
                     .build();
         }
         return instance;
     }
+
+    public abstract BookDao bookDao();
+    public abstract ChapterDao chapterDao();
+    public abstract BookProgressDao bookProgressDao();
 }
+
